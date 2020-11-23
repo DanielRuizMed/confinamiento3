22da6b2f1e0d1841753df86c3dc251125af0bbbb
DanielRuizMed <drm19933@gmail.com>
2020-11-23 18:38:07 +0100

    devolviendo json 6

--- Confinamiento/__init__.py
+++ Confinamiento/__init__.py
@@ -12,18 +12,18 @@ datos = json.loads(x)
 def main(req: func.HttpRequest) -> func.HttpResponse:
 
     ccaa = req.params.get('ccaa')
+    result = {}
 
     if ccaa:
-        result = datos.get(ccaa, 'No hay datos con esa comunidad o no existe')
+        result['result'] = datos.get(ccaa, 'No hay datos con esa comunidad o no existe')
         codigo = 200
 
     else:
-        result = "Nos has puesto bien los atributos, ejemplo: ?ccaa=Andalucia."
+        result['result'] = "Nos has puesto bien los atributos, ejemplo: ?ccaa=Andalucia."
         codigo = 404
 
-    
     return func.HttpResponse(
-        response,
+        json.dumps(result),
         mimetype="application/json",
         status_code=codigo
     )
